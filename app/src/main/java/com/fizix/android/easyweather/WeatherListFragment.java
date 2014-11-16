67f6720cb868d3ff942b35a5b55bc3651b936f54
Tiaan Louw <tiaanl@gmail.com>
2014-11-17 01:54:27 +0200

    Nicer placeholder for weather entry list.

--- app/src/main/java/com/fizix/android/easyweather/WeatherListFragment.java
+++ app/src/main/java/com/fizix/android/easyweather/WeatherListFragment.java
@@ -8,7 +8,6 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 public class WeatherListFragment extends Fragment {
 
@@ -16,6 +15,9 @@ public class WeatherListFragment extends Fragment {
 
     private long mLocationId;
     private RecyclerView mRecyclerView;
+
+    public WeatherListFragment() {
+    }
 
     public static WeatherListFragment newInstance(long locationId) {
         WeatherListFragment fragment = new WeatherListFragment();
@@ -26,9 +28,6 @@ public class WeatherListFragment extends Fragment {
         fragment.setArguments(args);
 
         return fragment;
-    }
-
-    public WeatherListFragment() {
     }
 
     @Override
@@ -51,7 +50,9 @@ public class WeatherListFragment extends Fragment {
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
         // Create a adapter for the list.
-        WeatherEntryAdapter adapter = new WeatherEntryAdapter(new String[]{"Item 1", "Item 2", "Item 3"});
+        WeatherEntryAdapter adapter = new WeatherEntryAdapter(
+                new String[]{"Item 1", "Item 2", "Item 3", "Item 4", "Item 5",
+                        "Item 6", "Item 7", "Item 8", "Item 9", "Item 10"});
         mRecyclerView.setAdapter(adapter);
 
         return rootView;
@@ -60,17 +61,6 @@ public class WeatherListFragment extends Fragment {
     public static class WeatherEntryAdapter extends RecyclerView.Adapter<WeatherEntryAdapter.ViewHolder> {
 
         private String[] mDataSet;
-
-        public static class ViewHolder extends RecyclerView.ViewHolder {
-
-            TextView title;
-
-            public ViewHolder(View parent) {
-                super(parent);
-                title = (TextView) parent.findViewById(R.id.title);
-            }
-
-        }
 
         public WeatherEntryAdapter(String[] dataSet) {
             mDataSet = dataSet;
@@ -85,12 +75,19 @@ public class WeatherListFragment extends Fragment {
 
         @Override
         public void onBindViewHolder(ViewHolder viewHolder, int position) {
-            viewHolder.title.setText(mDataSet[position]);
         }
 
         @Override
         public int getItemCount() {
             return mDataSet.length;
+        }
+
+        public static class ViewHolder extends RecyclerView.ViewHolder {
+
+            public ViewHolder(View parent) {
+                super(parent);
+            }
+
         }
 
     }
